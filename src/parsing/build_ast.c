aa992a9e0f29ba2d518358ff2f3213ee89ec13c4
brumaboi <brumastefan8@gmail.com>
2024-10-18 13:42:02 +0200

    added function to parse the token list until it meets an operator, also started building the structure of the AST

--- src/parsing/build_ast.c
+++ src/parsing/build_ast.c
@@ -12,6 +12,21 @@
 
 #include "../../inc/minishell.h"
 
+t_ast *create_cmd_node(char **cmd_args)
+{
+    t_ast *node;
+
+    node = malloc(sizeof(t_ast));
+    if (!node)
+        return (NULL); // need to see how to handle this error
+    node->type = N_COMMAND;
+    node->cmd_args = cmd_args;
+    node->file = NULL;
+    node->left = NULL;
+    node->right = NULL;
+    return (node);
+}
+
 t_ast *parse_command(t_token **current)
 {
     t_token *token;
@@ -22,8 +37,17 @@ t_ast *parse_command(t_token **current)
     args_count = 0;
     while(token && token->type == T_IDENTIFIER)
     {
+        cmd_args = ft_realloc(cmd_args, sizeof(char *) * (args_count + 2)); //we need to realloc the array because we don't know how many arguments we will have
+        if (!cmd_args)
+            return (NULL); // need to see how to handle this error
+        cmd_args[args_count] = ft_strdup(token->value); //we copy the value of the token in the array
+        if (!cmd_args[args_count])
+            return (NULL); // need to see how to handle this error
         token = token->next;
-    }   
+    }
+    cmd_args[args_count] = NULL; //we need to set the last element of the array to NULL
+    *current = token; //so it points to the last token processed
+    return (create_cmd_node(cmd_args));
 }
 
 t_ast *build_ast(t_token *tokens)
@@ -35,7 +59,7 @@ t_ast *build_ast(t_token *tokens)
     ast = parse_command(&current); //function to parse input until the next operator
     while(current)
     {
-           
+        
     }
     return (ast);
 }
