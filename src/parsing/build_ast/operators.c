841575a4452b19bd64b50be10c40f3caec1cbacb
brumaboi <brumastefan8@gmail.com>
2024-10-21 19:25:38 +0200

    added function that construct AST node for pipes

--- src/parsing/build_ast/operators.c
+++ src/parsing/build_ast/operators.c
@@ -33,10 +33,34 @@
 
 // }
 
-// t_ast parse_pipe(t_token **current, t_ast *ast)
-// {
+t_ast create_pipe_node(t_ast *ast, t_ast right)
+{
+    t_ast *node;
 
-// }
+    node = malloc(sizeof(t_ast));
+    if (!node)
+        return (NULL); // need to see how to handle this error
+    node->type = N_PIPE;
+    node->left = ast;
+    node->right = right;
+}
+
+t_ast parse_pipe(t_token **current, t_ast *ast)
+{
+    t_token *token;
+    t_ast *right;
+
+    token = *current;
+    if (token && token->type == T_PIPE)
+    {
+        token = token->next;
+        right = parse_command(&token);
+        if (!right)
+            return (NULL); // need to see how to handle this error
+        return(create_pipe_node(ast, right));
+    }
+    return (ast);
+}
 
 t_ast *parse_operators(t_token **current, t_ast *ast)
 {
