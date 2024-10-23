aa6436e3b8a309a6ce23f83e382b4e725bea4322
brumaboi <brumastefan8@gmail.com>
2024-10-23 10:38:36 +0200

    fixed up the signal function to correctly ignore sigquit

--- src/signal/signal.c
+++ src/signal/signal.c
@@ -21,15 +21,11 @@ void signal_handler(int signal)
         rl_replace_line("", 0); //replace the current line with nothing
         rl_redisplay(); //redisplay the prompt
     }
-    else if (signal == SIGQUIT)
-    {
-        return ;
-    }
 }
 
 //NEEDS WORK!!!!
 void init_signals(void)
 {
     signal(SIGINT, signal_handler); //ctrl + c
-    signal(SIGQUIT, signal_handler); //ctrl + /
+    signal(SIGQUIT, SIG_IGN); //ctrl + /
 }
