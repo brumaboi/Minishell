528d254e6817873d03904c7c806429eca372d7dd
brumaboi <brumastefan8@gmail.com>
2024-11-14 22:55:14 +0100

    made minimal changes to exe_builtin_env to make sure it works at all times

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -15,6 +15,7 @@
 int	exe_builtin_env(t_ast *ast, t_data *data)
 {
 	char **args;
+	t_var *current_env;
 
 	args = ast->cmd_args;
 	if (args[1])
@@ -22,17 +23,18 @@ int	exe_builtin_env(t_ast *ast, t_data *data)
 		printf("env: %s: no such file or directpry\n", args[1]);
 		return (1);
 	}
+	current_env = data->env;
 	data->env = data->env->next;
-	while (data->env)
+	while (current_env)
 	{
-		if (data->env->value) // to make sure NAME=VALUE format is printed
+		if (current_env->value) // to make sure NAME=VALUE format is printed
 		{
-			ft_putstr_fd(data->env->name, 1);
+			ft_putstr_fd(current_env->name, 1);
 			ft_putstr_fd("=", 1);
-			ft_putstr_fd(data->env->value, 1);
+			ft_putstr_fd(current_env->value, 1);
 			ft_putstr_fd("\n", 1);
 		}
-		data->env = data->env->next;
+		current_env = current_env->next;
 	}
 	return (0);
 }
