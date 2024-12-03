41793f3fb2f65d25f99640cccc4c24362ddecd4c
brumaboi <brumastefan8@gmail.com>
2024-12-03 16:10:48 +0100

    fractioned exit function to fit in the norm

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -12,10 +12,33 @@
 
 #include "../../inc/minishell.h"
 
-int exe_builtin_exit(t_ast *ast)
+void handle_exit(char **cmd_args)
 {
     int value;
-    
+
+    if (cmd_args[1])
+    {
+        value = ft_atoi(cmd_args[1]);
+        if (ft_isdigit(value))
+        {
+            exit(value);
+        }
+        else
+        {
+            printf("exit: %s numeric arg required\n", cmd_args[1]);
+            free_cmd_args(cmd_args);
+            exit(255);
+        }
+    }
+    else
+    {
+        free_cmd_args(cmd_args);
+        exit(0);
+    }
+}
+
+int exe_builtin_exit(t_ast *ast)
+{
     if (ast->cmd_args == NULL)
         return (1);
     if (ast->cmd_args[1] && ast->cmd_args[2])
@@ -24,24 +47,6 @@ int exe_builtin_exit(t_ast *ast)
         free_cmd_args(ast->cmd_args);
         exit(1);
     }
-    else if (ast->cmd_args[1])
-    {
-        value = ft_atoi(ast->cmd_args[1]);
-        if (ft_isdigit(value))
-        {
-            exit(value);
-        }
-        else
-        {
-            printf("exit: %s numeric arg required\n", ast->cmd_args[1]);
-            free_cmd_args(ast->cmd_args);
-            exit(255);
-        }
-    }
-    else
-    {
-        free_cmd_args(ast->cmd_args);
-        exit(0);
-    }
+    handle_exit(ast->cmd_args);
     return (0);
 }
