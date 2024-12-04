d04054c1b76b9428de8b60cf9fa9aaf2f82fd8e3
brumaboi <brumastefan8@gmail.com>
2024-12-04 23:09:49 +0100

    fractioned main function

--- src/main.c
+++ src/main.c
@@ -19,43 +19,55 @@ void cleanup_readline(void)
     rl_cleanup_after_signal();      // Cleanup after signal
 }
 
+void free_free(char *input, char **split_result, t_token *tokens, t_ast *ast)
+{
+    if (input)
+        free(input);
+    if (split_result)
+        free_split(split_result);
+    if (tokens)
+        free_tokens(tokens);
+    if (ast)
+        free_ast(ast);
+}
+
+int process_split(char *input, char ***split_result, t_token **tokens, t_data *data)
+{
+    int count;
+
+    count = 0;
+    *split_result = split_input(input, &count, tokens, data);
+    if (!split_result || !tokens)
+    {
+        free_free(input, *split_result, *tokens, NULL);
+        return (0);
+    }
+    return (1);
+}
 void process_input(t_data *data)
 {
-    t_token *tokens;
     char *input;
-    t_ast *ast;
-    int  count;
     char **split_result;
+    t_token *tokens;
+    t_ast *ast;
 
     tokens = NULL;
     input = NULL;
     ast = NULL;
     split_result = NULL;
-    count = 0;
     input = get_input(data);
     if (!input)
         return ;
-    split_result = split_input(input, &count, &tokens, data);
-    if (!split_result || !tokens)
-    {
-        free(input);
-        free_split(split_result);
-        free_tokens(tokens);
-        return;
-    }
+    if (!process_split(input, &split_result, &tokens, data))
+        return ;
     ast = build_ast(tokens);
     if (!ast)
     {
-        free_tokens(tokens);
-        free_split(split_result);
-        free(input);
+        free_free(input, split_result, tokens, NULL);
         return ;
     }
     execute_asts(ast, data);
-    free_ast(ast);
-    free_tokens(tokens);
-    free_split(split_result);
-    free(input);
+    free_free(input, split_result, tokens, ast);
 }
 
 int main(int argc, char **argv, char **env)
