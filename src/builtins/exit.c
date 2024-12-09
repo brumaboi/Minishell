c2851b8e1165ad2b8106d083171c5a8d9bbbd530
brumaboi <brumastefan8@gmail.com>
2024-12-09 23:34:20 +0100

    fixes around the exit builtins in relation to output and exit codes

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -14,8 +14,12 @@
 
 int is_numeric(const char *str)
 {
+    if (!str || !*str)
+        return (0);
     if (*str == '+' || *str == '-')
         str++;
+    if (!*str) // Handle cases like "+" or "-"
+        return (0);
     while (*str)
     {
         if (!ft_isdigit((unsigned char)*str))
@@ -37,21 +41,28 @@ int parse_exit_code(const char *str)
 
 void handle_exit(char **cmd_args)
 {
-    int exit_code = 0;
+    int exit_code;
 
+    exit_code = 0;
     if (cmd_args[1])
     {
         if (is_numeric(cmd_args[1]))
         {
+            if (cmd_args[2]) // Too many arguments
+            {
+                fprintf(stderr, " exit: too many arguments\n");
+                free_cmd_args(cmd_args);
+                return ; // Do not exit, return to shell
+            }
             exit_code = parse_exit_code(cmd_args[1]);
             free_cmd_args(cmd_args);
             exit(exit_code);
         }
         else
         {
-            printf("exit: %s numeric arg required\n", cmd_args[1]);
+            fprintf(stderr, " exit: %s: numeric argument required\n", cmd_args[1]);
             free_cmd_args(cmd_args);
-            exit(255);
+            exit(2);
         }
     }
     free_cmd_args(cmd_args);
@@ -62,11 +73,6 @@ int exe_builtin_exit(t_ast *ast)
 {
     if (ast->cmd_args == NULL)
         return (1);
-    if (ast->cmd_args[1] && ast->cmd_args[2])
-    {
-        printf("exit: too many arguments\n");
-        return (1);
-    }
-    handle_exit(ast->cmd_args);
+    handle_exit(ast->cmd_args); // Delegate the logic
     return (0);
 }
